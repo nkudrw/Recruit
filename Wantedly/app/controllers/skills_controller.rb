92b8706e3911c77daeb5d6b2b1a73f49068e4f7c
Naoki <naoki826divide@gmail.com>
2016-12-05 04:03:41 +0000

    UI

--- Wantedly/app/controllers/skills_controller.rb
+++ Wantedly/app/controllers/skills_controller.rb
@@ -10,10 +10,6 @@ class SkillsController < ApplicationController
   def create
       @skill = Skill.new(skills_params)
       @skill.save
-      format.html { redirect_to user_path(@skill.points.first.user_id) }
-       format.json { render :show, status: :created, location: @skill }
-       # JS形式でレスポンスを返します。
-       format.js { render :show }
 
   end
 
